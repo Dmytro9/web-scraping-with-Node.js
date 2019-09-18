7913c833d7c649617c21865af0044daeb06c83e8
Dmytro9 <dpoliuliuk@gmail.com>
2019-09-18 14:11:27 +0300

    selectors

--- scrapingintro/index.js
+++ scrapingintro/index.js
@@ -4,12 +4,13 @@ const cheerio = require('cheerio')
 
 
 async function main() {
-    const html = await request.get('https://reactnativetutorial.net/css-selectors/')
+    const html = await request.get('https://www.facebook.com/')
     fs.writeFileSync('./test.html', html)
 
-    const $ = await cheerio.load(html)
-    const theText = $('h1').text()
-    console.log('theText -', theText)
+    // const $ = await cheerio.load(html)
+    // $('h2').each((index, element) => {
+        // console.log($(element).text())
+    // })
 }
 
 main()
